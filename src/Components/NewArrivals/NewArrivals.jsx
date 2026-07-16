5c4eaadb4355ced294cf05f6f438696d6fa2e8a9
jahidhasan909 <jjjjahidhasan999@gmail.com>
2026-07-17 01:34:41 +0600

    new arrivals features section with grid configuration is done

---
+++ src/Components/NewArrivals/NewArrivals.jsx
@@ -0,0 +1,76 @@
+import Image from "next/image";
+import React from "react";
+
+const NewArrivals = () => {
+  const collections = [
+    {
+      id: 1,
+      title: "MAN Shirt Formal",
+      image:
+        "https://i.ibb.co.com/VY45gCJw/Product-Image-1769496706.jpg",
+    },
+    {
+      id: 2,
+      title: "MAN Ethnic Wear",
+      image:
+        "https://i.ibb.co.com/svB2013M/Product-Image-1773494262.jpg",
+    },
+    {
+      id: 3,
+      title: "women's western wear",
+      image:
+        "https://i.ibb.co.com/W4qr2vjM/DSC0031-22581bd5-7d8c-4452-823b-f7d1b6be6f82.webp",
+    },
+    {
+      id: 4,
+      title: "Summer Essentials",
+      image:
+        "https://i.ibb.co.com/TxQ5CFgm/Product-Image-1714122035.jpg",
+    },
+  ];
+
+  return (
+    <section className="mt-20">
+      <div className="text-center mb-12">
+        <h2 className="text-3xl md:text-4xl font-bold text-gray-900">
+          New Arrivals
+        </h2>
+
+        <p className="mt-3 text-gray-500 max-w-md mx-auto">
+          Discover our latest collection crafted for everyday elegance.
+        </p>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        {collections.map((item) => (
+          <div
+            key={item.id}
+            className="group relative overflow-hidden rounded-md bg-[#f8f6f3] shadow-sm"
+          >
+      
+            <div className="h-[520px] flex items-center justify-center overflow-hidden">
+              <Image
+                src={item.image}
+                alt={item.title}
+                width={700}
+                height={900}
+                className="w-full h-full object-cover object-top transition-transform duration-500 group-hover:scale-105"
+              />
+            </div>
+
+            
+            <div className="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black/70 via-black/20 to-transparent" />
+
+            <div className="absolute bottom-6 left-6">
+              <h3 className="text-xl md:text-2xl uppercase  font-semibold text-white">
+                {item.title}
+              </h3>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default NewArrivals;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/Components/Banner/Banner";
+import NewArrivals from "@/Components/NewArrivals/NewArrivals";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Banner></Banner>
+      <NewArrivals></NewArrivals>
     </div>
   );
 }
